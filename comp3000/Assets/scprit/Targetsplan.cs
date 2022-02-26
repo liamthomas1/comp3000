711a33341271883fd57d89e67621b41047c7d33d
liamthomas1 <55701056+liamthomas1@users.noreply.github.com>
2022-02-26 00:31:58 +0000

    usertest part 2
    add in the number systems for the user so they can see they are get point
    right now it only work when i press 1
    get ready as i want to do a user test by next 2 weeks

--- comp3000/Assets/scprit/Targetsplan.cs
+++ comp3000/Assets/scprit/Targetsplan.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Targetsplan : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    int test;
+    public Text scoretext;
     // Update is called once per frame
     void Update()
     {
         
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+            scoretext.text = "";
+            test = test + 1;
+            scoretext.text = test.ToString("0");
+		}
+
     }
 }
