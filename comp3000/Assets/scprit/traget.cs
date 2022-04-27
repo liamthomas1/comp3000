4431799f55cbb668ed46722b29a3a8bd766819cc
liamthomas1 <55701056+liamthomas1@users.noreply.github.com>
2022-04-27 21:47:32 +0100

    ground add colour

---
+++ comp3000/Assets/scprit/traget.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class traget : MonoBehaviour
+{
+    public GameObject tragetsspawn; // set the object https://answers.unity.com/questions/946210/random-spawn-position.html
+    public float tragethit;
+    public Transform[] spawnnewtraget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
