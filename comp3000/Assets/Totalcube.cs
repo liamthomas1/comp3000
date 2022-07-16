400c735a3383e5c642f2e95af22a38fc51d26303
liamthomas1 <55701056+liamthomas1@users.noreply.github.com>
2022-07-16 18:40:18 +0100

    cube
    add logic of the cube to the systems no code to check but set it so it will coneect with playmanger as well other systems

---
+++ comp3000/Assets/Totalcube.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Totalcube : MonoBehaviour
+{
+    int cube;
+    GameObject cubeneed,cubehitbox;
+    bool cubeistrue = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void cubecollect()
+	{
+        if (cubeistrue == true)
+		{
+
+		}
+	}
+    void cubehitboxcheck()
+	{
+
+	}
+}
