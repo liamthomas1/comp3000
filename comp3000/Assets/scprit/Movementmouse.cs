4e9ee2315ae91b6a58358bd73a1086ef10085399
liamthomas1 <55701056+liamthomas1@users.noreply.github.com>
2022-07-21 02:53:14 +0100

    change
    have to remove old VR stuff into a folder as it was causing problem with new game design
    menu work
    movement work
    now it just getting cube to work
    movement will get a user test soon

--- comp3000/Assets/scprit/Movementmouse.cs
+++ comp3000/Assets/scprit/Movementmouse.cs
@@ -5,27 +5,128 @@ using UnityEngine.UI;
 public class Movementmouse : MonoBehaviour
 {
     public CharacterController controller; // might need to remove thios for me 
-        public float speed = 12f;
+        public float speed = 0f; //overall speed
+    public float speedgain = 1f; // speed gain when button hit
+    public float speedlost = -1f; // speed lost when button hit
+    public float speedmax = 12f;
+    public float speedmaxunder = -12f;
     public Text speedtext;
-  
+    public Text timer;
+    public Text controlslayout;
+    public bool changecontrols = false;
     // Update is called once per frame 
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * 12 * Time.deltaTime);
-		if (Input.GetKeyDown(KeyCode.W))
-		{
-            Debug.Log("test");
-            SPEEDUP();
+        Movementunity();
+
+        if (Input.GetKeyDown(KeyCode.Tab)){
+            ChangesControlsBool();
 		}
+        if (changecontrols == true)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Debug.Log("test");
+                SPEEDUP();
+            }
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                slowdow();
+            }
+            if (Input.GetKeyDown(KeyCode.V))
+            {
+                returnzero();
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Debug.Log("test");
+                SPEEDUP();
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                slowdow();
+            }
+            if (Input.GetKeyDown(KeyCode.V))
+            {
+                returnzero();
+            }
+        }
         
     }
 
  void SPEEDUP()
 	{
-        speedtext.text =   speed.ToString("0.00"); //this is get right infomation
+        if (speed == speedmax)
+        {
+            timer.text = "you are at max speed";
+            speedtext.text = speed.ToString("0.00");
+        }
+        else
+        {
+            speed = speed + speedgain;
+            speedtext.text = speed.ToString("0.00"); //this is get right infomation
+            timer.text = "";
+        }
 	}
-    
+    void slowdow()
+	{
+        if (speed == speedmaxunder)
+        {
+           
+          timer.text = "you are at max speed";
+            
+            speedtext.text = speed.ToString("0.00");
+        }
+        else
+        {
+            speed = speed + speedlost;
+            speedtext.text = speed.ToString("0.00");
+            timer.text = "";
+        }
+	}
+    void setupkey()
+	{
+
+	}
+    void Movementunity()
+	{
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+    void returnzero()
+	{
+        speed = 0f;
+        speedtext.text = speed.ToString("0.00");
+        timer.text = "";
+	}
+    void ChangesControlsBool()
+	{
+        if (changecontrols == false)
+		{
+            changecontrols = true;
+            controlslayout.text = "R to increase speed\n" + 
+				 
+				"F to decrease speed\n" +
+				"C to Set speed 0\n" +
+                "name of this controls is:TWO";
+
+
+        }
+        else if (changecontrols == true)
+		{
+            changecontrols = false;
+            controlslayout.text = "F to increase speed\n" +
+
+               "R to decrease speed\n" +
+               "C to Set speed 0\n " +
+               "name of this control is:ONE \n"
+              ;
+        }
+	}
 }
+
---
+++ comp3000/Assets/scprit/TempMenu.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class TempMenu : MonoBehaviour
+{
+    public GameObject MENU;
+    public bool MENUon = false;
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+            //exitgame();
+            GAMETIME();
+		}
+    }
+    void GAMETIME()
+	{
+        if (MENUon == true)
+        {
+            MENUon = false;
+            Time.timeScale = 1;
+        }
+        else if(MENUon == false){
+            MENUon = true;
+            Time.timeScale = 0;
+		}
+	}
+   public void exitgame()
+	{
+        Application.Quit();
+	}
+    public void startgame()
+	{
+        SceneManager.LoadScene(1);
+	}
+   public void Exittomenu()
+	{
+        SceneManager.LoadScene(0);
+	}
+}
